65d1e209188b4c67d4386e91c0dbea67cb9d4dd3
Andreas Riemer <andreas.riemer@gmail.com>
2021-02-04 19:10:07 +0100

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Shared.MixProject do
   def project do
     [
       app: :jehovakel_ex_event_store,
-      version: "2.0.0",
+      version: "2.0.1",
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
